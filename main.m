391307e45662625756138f724534d3f7d32d133b
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-05-29 10:47:47 +0200

    Aggiunta dei plot e verifica raggiungibilità

--- main.m
+++ main.m
@@ -1,4 +1,16 @@
-% Definizioni costanti
+%%
+
+clear;
+clc;
+close all
+set(0,'DefaultLineLineWidth', 1.5);
+set(0,'defaultAxesFontSize', 14)
+set(0,'DefaultFigureWindowStyle', 'docked') 
+% set(0,'DefaultFigureWindowStyle', 'normal')
+set(0,'defaulttextInterpreter','latex')
+rng('default');
+
+%% Definizioni costanti
 T_ext = 278;
 k_ext = 9;
 
@@ -6,9 +18,11 @@ C = [6300;4600;4200];
 k = [16;18;19];
 tau = [580;520;540];
 
+
+%forse meglio mettere un vettore da 3 (abbiamo 3 ingressi)
 u = [0; 0 ; 0 ; 100 ; 100 ; 100 ];
 
-% ODE del sistema
+%% ODE del sistema
 dxdt = @(t,x) tempCasa(t, x, k, C, tau, T_ext, k_ext, u);
 
 
@@ -22,19 +36,49 @@ x0 = [280 285 290 0 0 0; 250 255 260 25 50 100; 285 290 295 25 50 100]';
 
 [tt, xx] = ode45(dxdt, [0 5000], x0(:, 1));
 figure
-plot(tt, xx);
+hold on
+subplot(2,1,1)
+plot(tt, xx(: , 1:3));
+title("Temperature delle stanze")
+legend(["T1" "T2" "T3"])
 
-% Linearizzazione
-A_lin = [-eye(3)*k_ext eye(3); zeros(3,3) -eye(3)./tau];
+subplot(2, 1,2)
+plot(tt , xx(: , 4:6));
+title("Potenza termica dei termosifoni")
+legend(["Q1" "Q2" "Q3"])
+hold off
+
+%% Linearizzazione
+
+A_lin = [-eye(3)*k_ext, eye(3); zeros(3,3), -eye(3)./tau];
 B_lin = [0 0 0 1/tau(1) 1/tau(2) 1/tau(3)]';
 C_lin = eye(6);
 D_lin = zeros(6,1);
 
 sys_lineare = ss(A_lin, B_lin, C_lin, D_lin);
 
+
 % Discretizziamo
 Ts = 60;
 sys_discretizzato = c2d(sys_lineare, Ts);
+figure
 pzmap(sys_discretizzato)
+
+%% analisi della raggiungibilità
+
+Mr_lineare = ctrb(sys_lineare);
+Mr_discretizzato = ctrb(sys_discretizzato);
+
+
+disp("Rango matrice di raggiungibilità sitema linearizzato:")
+disp(rank(Mr_lineare))
+disp("Dimensioni:")
+disp(width(Mr_lineare) + " x " + height(Mr_lineare))
+
+disp(" ")
+disp("Rango matrice di raggiungibilità sitema discretizzato:")
+disp(rank(Mr_discretizzato))
+disp("Dimensioni:")
+disp(width(Mr_discretizzato) + " x " + height(Mr_discretizzato))
 
 %PUSH
