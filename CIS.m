9f6ea9c16e3ea81d8beb344f6641522816d7c154
matteofalabretti <matteofalabretti@outlook.com>
2024-05-30 22:19:07 +0200

    correzione typo descrizione formula

--- CIS.m
+++ CIS.m
@@ -5,12 +5,10 @@ function [G, g] = CIS(A, B, x_ref, u_ref, Hx, hx, Hu, hu, Q, R)
 %   all'interno del CIS
 %   Input:
 %       - A, B: matrici del sistema
-%       - x_ref: equilibrio attorno al quale costruire il CIS
-%       - Hx*x<=hx: vincoli sullo stato
-%       - Hu*u<=hu: vincoli sull'ingresso
+%       - x_ref u_ref: equilibrio attorno al quale costruire il CIS
+%       - Hx*x <= hx: vincoli sullo stato
+%       - Hu*u <= hu: vincoli sull'ingresso
 %       - Q,R: matrici per LQR
-
-max_iteration = 10000;
 
 %   Controllore LQR
 K = -dlqr(A, B, Q, R);
@@ -23,7 +21,6 @@ H = [Hx;Hu * K];
 h = [hx;hu + Hu *(K*x_ref - u_ref)];
 
 %   Calcolo del CIS (G*x<=g)
-
 CIS_poly_curr = Polyhedron(H, h);
 continua = 1;
 i = 0;
@@ -32,14 +29,16 @@ tic
 
 while continua
     i = i+1;
+
     %   Memorizza vecchio candidato
     CIS_poly_prev = CIS_poly_curr;
     
     %   Calcola nuovo candidato (G_hat * x <= g_hat)
     G_hat = [CIS_poly_curr.A * A_lqr;H];
     g_hat = [CIS_poly_curr.b + CIS_poly_curr.A*B*(K*x_ref - u_ref);h];
+
     CIS_poly_curr= Polyhedron(G_hat, g_hat);
-    CIS_poly_curr =  minHRep(CIS_poly_curr); %riduce alla rappresentazioneminima del
+    CIS_poly_curr =  minHRep(CIS_poly_curr); %riduce alla rappresentazione minima del
     %Poliedro
     
     %plot di verifica
