377dd25f4e819e6f1bfe08db6a074e60ffd178ed
matteofalabretti <matteofalabretti@outlook.com>
2024-05-30 10:00:49 +0200

    definizione vincoli

---
+++ CIS.m
@@ -0,0 +1,47 @@
+function [G, g] = CIS(A, B, x_ref, u_ref, Fx, fx, Fu, fu, Q, R)
+max_iteration = 10000;
+%CIS Calcolo del control invariant set (CIS) di un sistema lineare
+%   Questo metodo assume che un controllore LQR venga utilizzato
+%   all'interno del CIS
+%   Input:
+%       - A, B: matrici del sistema
+%       - x_ref: equilibrio attorno al quale costruire il CIS
+%       - Fx*x<=fx: vincoli sullo stato
+%       - Fu*u<=fu: vincoli sull'ingresso
+%       - Q,R: matrici per LQR
+
+%   Controllore LQR
+K = -dlqr(A, B, Q, R);
+
+%   Matrice A del sistema controllato con LQR
+A_lrq = A + B*K;
+
+%   Vincoli sullo stato e sull'ingresso (F*x <= f)
+F = [Fx;Fu*K];
+f = [fx;fu + Fu*(K*x_ref - u_ref)];
+
+%   Calcolo del CIS (G*x<=g)
+CIS_poly_prev = Polyhedron();
+CIS_poly_curr = Polyhedron(F, f);
+i = 0;
+
+while CIS_poly_prev.isEmptySet || CIS_poly_prev ~= CIS_poly_curr || i > max_iteration
+    i = i+1;
+    
+    %   Memorizza vecchio candidato
+    CIS_poly_prev = CIS_poly_curr;
+    
+    %   Calcola nuovo candidato (G_hat * x <= g_hat)
+    G_hat = [CIS_poly_curr.A * A_lrq;F];
+    g_hat = [CIS_poly_curr.B + CIS_poly_curr.A*B*(K*x_ref - u_ref);f];
+    CIS_poly_curr= Polyhedron(G_hat, g_hat);
+    %minHRep(CIS_poly_curr); riduce alla rappresentazioneminima del
+    %Poliedro
+    
+end
+
+%   Disequazioni che descrivono il CIS
+G = CIS_poly_curr.A;
+g = CIS_poly_curr.B;
+
+end
--- main.m
+++ main.m
@@ -18,13 +18,15 @@ C = [6300;4600;4200];
 k = [16;18;19];
 tau = [580;520;540];
 
-
+% Definizione Obbiettivi di Controllo
+x_ref = [289 289 289 100 100 100];
 u = [0; 0 ; 0 ; 100 ; 100 ; 100 ];
+
 
 %% ODE del sistema
 dxdt = @(t,x) tempCasa(t, x, k, C, tau, T_ext, k_ext, u);
 
-
+% simulazione del comportamento del sistema al variare degli stati
 x0 = [280 285 290 0 0 0; 250 255 260 25 50 100; 285 290 295 25 50 100]';
 
 for i= 1:3
@@ -59,7 +61,7 @@ D_lin = zeros(6,1);
 
 sys_lineare = ss(A_lin, B_lin, C_lin, D_lin);
 
-% verifica stabilità
+% Verifica della Stabilità del sistema lineare
 disp("Autovalori di della matrice A linearizzata:")
 disp(eig(A_lin));
 
@@ -70,24 +72,41 @@ sys_discretizzato = c2d(sys_lineare, Ts);
 figure
 pzmap(sys_discretizzato)
 
-% verifica stabilità
+% Verifica della Stabilità del sistema discretizzato
 disp("Modulo degli autovalori di della matrice A discretizzata:")
 disp(abs(eig(sys_discretizzato.A)));
 
 
 %% analisi della raggiungibilità
 
+% computazione delle matici di raggiungibilità
 Mr_lineare = ctrb(sys_lineare);
 Mr_discretizzato = ctrb(sys_discretizzato);
 
+disp("Rango matrice di raggiungibilità sitema linearizzato: " + rank(Mr_lineare))
+%disp(rank(Mr_lineare))
+disp("Dimensioni: " + width(Mr_lineare) + " x " + height(Mr_lineare))
+%disp(width(Mr_lineare) + " x " + height(Mr_lineare))
 
-disp("Rango matrice di raggiungibilità sitema linearizzato:")
-disp(rank(Mr_lineare))
-disp("Dimensioni:")
-disp(width(Mr_lineare) + " x " + height(Mr_lineare))
+disp("------------------------------------------------------------")
+disp("Rango matrice di raggiungibilità sitema discretizzato: " + rank(Mr_discretizzato))
+%disp(rank(Mr_discretizzato))
+disp("Dimensioni: " + width(Mr_discretizzato) + " x " + height(Mr_discretizzato))
+%disp(width(Mr_discretizzato) + " x " + height(Mr_discretizzato))
 
-disp(" ")
-disp("Rango matrice di raggiungibilità sitema discretizzato:")
-disp(rank(Mr_discretizzato))
-disp("Dimensioni:")
-disp(width(Mr_discretizzato) + " x " + height(Mr_discretizzato))
+%% Definizione dei vincoli su stato e su ingressi
+%U_vinc = [0 150]; % [W]
+%X_vinc = [282.5 300]; % [K]
+
+Hx = [eye(6);-eye(6)];
+hx = [300*ones(3,1); 150*ones(3,1); 282*ones(3,1); zeros(3,1)];
+Hu = [eye(3), -eye(3)];
+hu = [150*ones(3,1); zeros(3,1)];
+
+%% definizione delle matrici del costo quadratico
+Q = eye(6);
+R = ones(6,1);
+
+%% Verifica dell'esistenza del Controllable Invariant Set
+
+[G, g]= CIS(sys_discretizzato.A, sys_discretizzato.B, x_ref, u, Hx, hx, Hu, hu, Q, R);
