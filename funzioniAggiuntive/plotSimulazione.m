efe2349e5aea9a2d2f9f805728ec85d4d807cedf
DecaMeron23 <e.meroni1@studenti.unibg.it>
2024-06-14 14:16:06 +0200

    Risposta con i plot

--- funzioniAggiuntive/plotSimulazione.m
+++ funzioniAggiuntive/plotSimulazione.m
@@ -1,11 +1,11 @@
-function plotSimulazione(tempo, storia_x  , storia_u , x_ref)
+function [plot_T , plot_Q , plot_U]  = plotSimulazione(tempo, storia_x  , storia_u , x_ref)
     tempo = tempo/60; %[min]
 
     figure
     
     sgtitle("Evoluzioni degli stati")
     
-    subplot(2 , 1, 1)
+    plot_T = subplot(2 , 1, 1);
     plot( tempo, storia_x(1:3 , :))
     yline(x_ref(1))
     legend(["T1" , "T2" , "T3" ,"Obbiettivo"])
@@ -13,7 +13,7 @@ function plotSimulazione(tempo, storia_x  , storia_u , x_ref)
     xlabel("Tempo $[min]$" , Interpreter="latex");
     title("Temperatura")
     
-    subplot(2 , 1, 2)
+    plot_Q = subplot(2 , 1, 2);
     plot(tempo, storia_x(4:end , :))
     yline(x_ref(4))
     ylim([0 , 150])
@@ -22,9 +22,8 @@ function plotSimulazione(tempo, storia_x  , storia_u , x_ref)
     xlabel("Tempo $[min]$" , Interpreter="latex");
     title("Potenza dei termosifoni")
     
-    
     figure
-    plot(tempo, storia_u)
+    plot_U = plot(tempo, storia_u);
     title("Azioni di controllo")
     ylabel("Potenza $[W]$" , Interpreter="latex");
     xlabel("Tempo $[min]$" , Interpreter="latex");
